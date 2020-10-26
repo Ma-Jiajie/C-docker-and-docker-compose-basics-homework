2ec05435b0d0d0e26a60787582347fc07de85b00
jiajie <jiajie.ma@thoughtworks.com>
2020-10-26 23:53:45 +0800

    [jiajie]add Dockerfile for alice

--- alice/src/main/java/com/thoughtworks/capability/gtb/alice/AliceController.java
+++ alice/src/main/java/com/thoughtworks/capability/gtb/alice/AliceController.java
@@ -2,12 +2,16 @@ package com.thoughtworks.capability.gtb.alice;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class AliceController {
 
-    @GetMapping("/hello")
-    public String bob() {
-        return "";
+    @GetMapping(path = "/hello")
+    public String hello(){
+        String requestToBobUrl = "http://bob:8081/hello";
+
+        RestTemplate restTemplate = new RestTemplate();
+        return restTemplate.getForObject(requestToBobUrl, String.class);
     }
 }
